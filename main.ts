746372f9fb820dcb2eb649f0f66d1376b56dae41
mehbark <terezi@pyrope.net>
2024-01-09 18:40:40 -0500

    leader boards!

--- main.ts
+++ main.ts
@@ -6,22 +6,62 @@ import * as csv from "https://deno.land/std@0.211.0/csv/mod.ts";
 
 const FACTION_SHEET_CSV_URL =
   "https://docs.google.com/spreadsheets/d/1yxy672ufBpg2R-aUqtpKI5erArzsqL0SjJcRgHKPhEo/export?format=csv&id=1yxy672ufBpg2R-aUqtpKI5erArzsqL0SjJcRgHKPhEo&gid=2066926104";
-const LEADERBOARD_SHEET_CSV_URL =
-  "https://docs.google.com/spreadsheets/d/1yxy672ufBpg2R-aUqtpKI5erArzsqL0SjJcRgHKPhEo/export?format=csv&id=1yxy672ufBpg2R-aUqtpKI5erArzsqL0SjJcRgHKPhEo&gid=463315644";
+
+// HACK: least futureproof thing by far
+// also just not fun to fill out
+// i'm using a mix of the main(?) "Root Digital League 3.0" sheet and the Dev/Community one
+const LEADERBOARD_SHEET_CSV_URLS = [
+  // main
+  {
+    name: "All Seasons",
+    url:
+      "https://docs.google.com/spreadsheets/d/1yxy672ufBpg2R-aUqtpKI5erArzsqL0SjJcRgHKPhEo/export?format=csv&id=1yxy672ufBpg2R-aUqtpKI5erArzsqL0SjJcRgHKPhEo&gid=463315644",
+  },
+  // dev
+  {
+    name: "Season A01",
+    url:
+      "https://docs.google.com/spreadsheets/d/1WW7UXztxE4qgUtGSYlJClShni0AMhQUCnbv2DoIyC1c/export?format=csv&id=1WW7UXztxE4qgUtGSYlJClShni0AMhQUCnbv2DoIyC1c&gid=169812129",
+  },
+  {
+    name: "Season A02",
+    url:
+      "https://docs.google.com/spreadsheets/d/1WW7UXztxE4qgUtGSYlJClShni0AMhQUCnbv2DoIyC1c/export?format=csv&id=1WW7UXztxE4qgUtGSYlJClShni0AMhQUCnbv2DoIyC1c&gid=160732981",
+  },
+  {
+    name: "Season A03",
+    url:
+      "https://docs.google.com/spreadsheets/d/1WW7UXztxE4qgUtGSYlJClShni0AMhQUCnbv2DoIyC1c/export?format=csv&id=1WW7UXztxE4qgUtGSYlJClShni0AMhQUCnbv2DoIyC1c&gid=1084691973",
+  },
+  {
+    name: "Season A04",
+    url:
+      "https://docs.google.com/spreadsheets/d/1WW7UXztxE4qgUtGSYlJClShni0AMhQUCnbv2DoIyC1c/export?format=csv&id=1WW7UXztxE4qgUtGSYlJClShni0AMhQUCnbv2DoIyC1c&gid=1204325811",
+  },
+  // main
+  {
+    name: "Season A05",
+    url:
+      "https://docs.google.com/spreadsheets/d/1yxy672ufBpg2R-aUqtpKI5erArzsqL0SjJcRgHKPhEo/export?format=csv&id=1yxy672ufBpg2R-aUqtpKI5erArzsqL0SjJcRgHKPhEo&gid=1225920116",
+  },
+] satisfies {
+  name: SeasonName;
+  url: CsvSheetUrl;
+}[];
+
+// overly strict for fun; low chance of actually catching mistakes
+type CsvSheetUrl =
+  `https://docs.google.com/spreadsheets/d/${string}/export?format=csv&id=${string}`;
 
 let last_fetched = new Date("1970-01-01");
-// 5 minutes
-const MAX_AGE_MS = 5 * 60 * 1000;
+// 1 minute
+const MAX_AGE_MS = 1 * 60 * 1000;
 
 let cached_faction_csv: string[][] = [];
-let cached_leader_board_csv: string[][] = [];
+let cached_leader_board_csvs: { name: SeasonName; csv: string[][] }[] = [];
 
 const should_refetch = () =>
   (new Date().getTime() - last_fetched.getTime()) > MAX_AGE_MS;
-
-// wordy, but explicit
-let cached_faction_stats: FactionStats = {};
-let cached_leader_board: LeaderBoard = [];
 
 type Faction = {
   order: number;
@@ -80,8 +120,8 @@ const season_faction_stats = (
 const season_names = () =>
   cached_faction_csv.slice(19).map((s) => `Season ${s[1]}` as SeasonName);
 
-// let it crash! this is erlang right?
-function mk_faction_stats(csv: string[][]): FactionStats {
+function faction_stats(): FactionStats {
+  const csv = cached_faction_csv;
   const factions = csv[1].slice(1).filter(Boolean);
   const seasons_section = csv.slice(19);
 
@@ -111,67 +151,102 @@ function mk_faction_stats(csv: string[][]): FactionStats {
 
   return stats;
 }
+function leader(row: string[]): Leader {
+  const gamesPlayed = parseInt(row[2]);
+  const leagueScore = parseFloat(row[3]);
+  return {
+    rank: parseInt(row[0]),
+    player: row[1],
+    gamesPlayed,
+    leagueScore,
+    winRate: leagueScore / gamesPlayed,
+  };
+}
 
-function mk_leader_board(csv: string[][]): LeaderBoard {
-  return null as any;
+function leader_board_season(
+  { name, csv }: { name: SeasonName; csv: string[][] },
+  order: number,
+): LeaderBoardSeason {
+  const currentThreshold = parseInt(csv[0][8]);
+  return {
+    name,
+    currentThreshold,
+    targetThreshold: currentThreshold,
+    order,
+    leaders: csv.slice(2).map(leader),
+  };
+}
+
+const leader_boards = (): LeaderBoard =>
+  cached_leader_board_csvs.map(leader_board_season);
+
+function player_stats(name: string): string {
+  return "//TODO";
 }
 
 const update_csvs = async () => {
+  console.log(`[${new Date().toISOString()}] updating CSVs...`);
   cached_faction_csv = csv.parse(
     await fetch(
       FACTION_SHEET_CSV_URL,
       {},
     ).then((r) => r.text()),
   );
-  cached_leader_board_csv = csv.parse(
-    await fetch(
-      LEADERBOARD_SHEET_CSV_URL,
-      {},
-    ).then((r) => r.text()),
+  cached_leader_board_csvs = await Promise.all(
+    LEADERBOARD_SHEET_CSV_URLS.map(async (
+      { name, url },
+    ) => ({
+      name,
+      csv: csv.parse(
+        await fetch(url).then((r) => r.text()),
+      ),
+    })),
   );
+  console.log(`[${new Date().toISOString()}] CSVs updated!`);
 };
 
 const refetch = async () => {
   last_fetched = new Date();
   await update_csvs();
-  cached_faction_stats = mk_faction_stats(cached_faction_csv);
-  cached_leader_board = mk_leader_board(cached_leader_board_csv);
 };
-
-// const faction_stats = () => {
-//   if (should_refetch()) {
-//     update_stats();
-//   }
-//   return cached_faction_stats;
-// };
 
 await refetch();
 
-// CAVEAT:
-// the above is refreshingly simple, but obscures some complexity in how *precisely* stats are distributed
-// on each request, if it has been more than MAX_AGE_MS, we will update the stats,
-// !but we return the old stats immediately!
-// it shouldn't be a big deal (low volume), but it is worth keeping in mind that this is a *tradeoff*
-
 // TODO: a bit more fault-tolerance
 // TODO: PER PLAYER STATS! COMPLETELY DIFFERENT, DO NOT FORGET
-// i think we should cache the csv for those and generate them on demand
+// apparently that's under faction stats. good to know!
 Deno.serve((req) => {
   if (should_refetch()) {
     queueMicrotask(refetch);
   }
 
-  // simple regex is still appropriate for now i believe
-  if (/\/faction-stats\/?$/.test(req.url)) {
-    // how did i forget about Response.json?? so nice not having to get the header just right
-    return Response.json(cached_faction_stats, {
+  const url = new URL(req.url);
+
+  if (/^\/faction-stats\/?/.test(url.pathname)) {
+    return Response.json(faction_stats(), {
       headers: { "access-control-allow-origin": "*" },
     });
-  } else if (/\/leader-board\/?$/.test(req.url)) {
-    return new Response(JSON.stringify(cached_leader_board), {
-      headers: { "access-control-allow-origin": "*" },
-    });
-  } else {
-    return new Response("not found", { status: 404 });
   }
+
+  if (/^\/player\/?/.test(url.pathname)) {
+    const name = url.searchParams.get("name");
+    if (name) {
+      return Response.json(player_stats(name), {
+        headers: { "access-control-allow-origin": "*" },
+      });
+    } else {
+      return Response.json({ "error": "must give name param" }, {
+        status: 400,
+        headers: { "access-control-allow-origin": "*" },
+      });
+    }
+  }
+
+  if (/\/leader-boards\/?$/.test(url.pathname)) {
+    return Response.json(leader_boards(), {
+      headers: { "access-control-allow-origin": "*" },
+    });
+  }
+
+  return new Response("not found", { status: 404 });
 });
